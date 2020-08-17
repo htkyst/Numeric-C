a8405721dce12c925864ddac2894bd38465eb713
htkyst <rubybox@outlook.jp>
2020-08-17 22:04:36 +0900

    [Add] index

--- util.h
+++ util.h
@@ -3,5 +3,15 @@
 #define _UTILS_H_
 
 static float sqrarg;
+#define SQR(a) ((sqrarg=(a)) == 0.0 ? 0.0 : sqrarg*sqrarg)
+
+static double dsqrarg;
+#define DSQR(a) ((dsqrarg=(a)) == 0.0 ? 0.0 : dsqrarg*dsqrarg)
+
+static double dmaxarg1, dmaxarg2;
+#define DMAX(a,b) (dmaxarg1=(a), dmaxarg2=(b), \
+		(dmaxarg1) > (dmaxarg2) ? (dmaxarg1) : (dmaxarg2))
+
+
 
 #endif
